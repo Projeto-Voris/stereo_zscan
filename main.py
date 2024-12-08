0a7b08b5981501ff7b5b3fbd26a825120cdc45ed
Daniel Regner <daniel_regner@hotmail.com>
2024-12-08 09:21:19 -0300

    feat: split second as "octree"

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from extras.project_points import read_images, points3d_cube
 
 
 def main():
-    yaml_file = 'cfg/SM4_20241004_bianca.yaml'
-    images_path = 'images/SM4-20241004 - noise'
+    yaml_file = 'cfg/SM4_20241018_bouget.yaml'
+    images_path = 'images/SM4-20241112 - close'
 
     Nimg = 40
     fringe_image_name = '016.csv'
@@ -38,39 +38,48 @@ def main():
         t2 = time.time()
         Zscan = InverseTriangulation(yaml_file=yaml_file)
         Zscan.read_images(right_imgs=right_images[:, :, :n_img], left_imgs=left_images[:, :, :n_img])
-        points_3d = Zscan.points3d(x_lim=(-300, 350), y_lim=(-400, 400), z_lim=(-800, 400), xy_step=15, z_step=2,
+        points_3d = Zscan.points3d(x_lim=(-600, 600), y_lim=(-600, 600), z_lim=(-800, 800), xy_step=15, z_step=2,
                                    visualize=False)
         print('3D meshgrid pts: {} mi '.format(points_3d.shape[0]/1e6))
         print('Create mesgrid pcl: {} s'.format(round(time.time() - t2, 2)))
         t3 = time.time()
 
-        correl_points = Zscan.correlation_process(points_3d=points_3d, visualize=False, save_points=False,
+        correl_points = Zscan.correlation_process(points_3d=points_3d, visualize=True, save_points=False,
                                                   win_size=7, threshold=0.95)
         print('First Correl {}'.format(round(time.time() - t3, 2)))
         t4 = time.time()
 
         xlim, ylim, zlim = [min(correl_points[:,0]), max(correl_points[:,0])], [min(correl_points[:,1]), max(correl_points[:,1])], [min(correl_points[:,2]), max(correl_points[:,2])]
-        points_3d_2 = Zscan.points3d(x_lim=xlim, y_lim=ylim, z_lim=zlim, z_step=.5, xy_step=1, visualize=False)
-
+        points_3d_2 = Zscan.points3d(x_lim=xlim, y_lim=ylim, z_lim=zlim, z_step=.5, xy_step=5, visualize=False)
+        print('X: {}x{}, Y: {}x{}, Z:{}x{}'.format(xlim[0], xlim[1], ylim[0], ylim[1], zlim[0], zlim[1]))
         print('2nd 3D meshgrid pts: {} mi'.format(points_3d_2.shape[0]/1e6))
         print('Create second meshgrid pcl: {} s'.format(round(time.time() - t4, 2)))
         t4 = time.time()
-        correl_points = Zscan.correlation_process(points_3d=points_3d_2, visualize=False, save_points=False,
-                                                  win_size=7, threshold=0.9)
+        correl_points = Zscan.correlation_process(points_3d=points_3d_2, visualize=True, save_points=False,
+                                                  win_size=7, threshold=0.8)
+
         print('Second Correl {}'.format(round(time.time() - t4, 2)))
+        split_correl = np.array_split(correl_points, 6)
+        output_3d = []
+        for i, split in enumerate(split_correl):
+            xlim, ylim, zlim = [min(split[:,0]), max(split[:,0])], [min(split[:,1]), max(split[:,1])], [min(split[:,2]), max(split[:,2])]
+            points_3d = Zscan.points3d(x_lim=xlim, y_lim=ylim, z_lim=zlim, z_step=.01, xy_step=1, visualize=False)
+            print('{} - X: {}x{}, Y: {}x{}, Z:{}x{}'.format(i, xlim[0], xlim[1], ylim[0], ylim[1], zlim[0], zlim[1]))
+            print('{} - 3D meshgrid pts: {} mi'.format(i,points_3d.shape[0]/1e6))
+            correl_points = Zscan.correlation_process(points_3d=points_3d, visualize=True, save_points=False,
+                                                      win_size=7, threshold=0.8)
+            output_3d.append(correl_points)
+        print('wait')
 
 
     print('Full time: {} s'.format(round(time.time() - t0, 2)))
 
-    Zscan.save_points(correl_points, filename='./sm4_parede_win7.csv')
+    # Zscan.save_points(correl_points, filename='./sm4_parede_win7.csv')
 
-    # Inverse Triangulation for correlation
-    # Zscan.read_images(left_imgs=left_images, right_imgs=right_images)
-    # Zscan.correlation_process(win_size=5, correl_param=(0.3, 0.7), save_points=False, visualize=True)
 
-    # Inverse Triangulation for fringe projection
-    # Zscan.read_images(left_imgs=left_image, right_imgs=right_image)
-    # z_zcan_points = Zscan.fringe_process(save_points=True, visualize=True)
+
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == "__main__":
